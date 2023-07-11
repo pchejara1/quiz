96791b9b38c6345aa4ab42cfda445aba3eff784c
priyanka chejara <priyankachejara@gmcads.com>
2023-07-11 10:22:15 +0530

    final 1st draft of design completed

---
+++ assets/js/home.js
@@ -0,0 +1,7 @@
+// function openNav() {
+//     document.getElementById("mobileNav").style.width = "300px";
+//   }
+  
+//   function closeNav() {
+//     document.getElementById("mobileNav").style.width = "0";
+//   }
